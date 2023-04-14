4ff1e4d5dafbc29d4f81423b8cfe7ab5a0384451
kyunkyuu <teguh.iqbal782@gmail.com>
2023-04-14 19:08:34 +0700

    finalize

--- lib/presentation/home_screen/home_screen.dart
+++ lib/presentation/home_screen/home_screen.dart
@@ -5,9 +5,7 @@ import 'package:teguh_iqbal_s_application1/core/app_export.dart';
 import 'package:teguh_iqbal_s_application1/widgets/app_bar/custom_app_bar.dart';
 import 'package:teguh_iqbal_s_application1/widgets/custom_search_view.dart';
 import 'package:teguh_iqbal_s_application1/widgets/custom_text_form_field.dart';
-// ignore_for_file: must_be_immutable
 
-// ignore_for_file: must_be_immutable
 class HomeScreen extends StatelessWidget {
   TextEditingController frame198Controller = TextEditingController();
 
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:teguh_iqbal_s_application1/presentation/detail_task_detail_screen/detail_task_detail_screen.dart';
 import 'package:teguh_iqbal_s_application1/presentation/home_screen/home_screen.dart';
 import 'package:teguh_iqbal_s_application1/presentation/login_screen/login_screen.dart';
-import 'package:teguh_iqbal_s_application1/presentation/task_screen/task_screen.dart';
-import 'package:teguh_iqbal_s_application1/presentation/detail_task_progress_screen/detail_task_progress_screen.dart';
-import 'package:teguh_iqbal_s_application1/presentation/detail_task_chat_screen/detail_task_chat_screen.dart';
-import 'package:teguh_iqbal_s_application1/presentation/create_task_screen/create_task_screen.dart';
 import 'package:teguh_iqbal_s_application1/presentation/app_navigation_screen/app_navigation_screen.dart';
 
 class AppRoutes {
@@ -26,13 +21,8 @@ class AppRoutes {
   static const String appNavigationScreen = '/app_navigation_screen';
 
   static Map<String, WidgetBuilder> routes = {
-    detailTaskDetailScreen: (context) => DetailTaskDetailScreen(),
     homeScreen: (context) => HomeScreen(),
     loginScreen: (context) => LoginScreen(),
-    taskScreen: (context) => TaskScreen(),
-    detailTaskProgressScreen: (context) => DetailTaskProgressScreen(),
-    detailTaskChatScreen: (context) => DetailTaskChatScreen(),
-    createTaskScreen: (context) => CreateTaskScreen(),
     appNavigationScreen: (context) => AppNavigationScreen()
   };
 }
